2a2e0b52c48f723006dbcdde9c402227752462cb
Alme <davicampoialmemarcenaria@gmail.com>
2026-02-20 10:13:05 -0300

    serving frontend

--- main.py
+++ main.py
@@ -1,21 +1,26 @@
+import os
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import FileResponse
+from fastapi.staticfiles import StaticFiles
 from pydantic import BaseModel
 import psycopg2
 
 app = FastAPI()
 
-# ⚠️ CONFIRA SE A PORTA É 6543 NO SEU SUPABASE
-DATABASE_URL = "postgresql://postgres.fkpnzuygmqdbfsrxhmdg:Alme%24202510M**--@aws-1-sa-east-1.pooler.supabase.com:54332/postgres"
+# Servir arquivos estáticos
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
-# 🔓 LIBERAR CORS (IMPORTANTE PARA FUNCIONAR NO FRONTEND)
+# Liberar CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # depois podemos restringir
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 class Lead(BaseModel):
     nome: str
@@ -24,9 +29,10 @@ class Lead(BaseModel):
     perfil: str
     investimento: str
 
+# 👇 AQUI FAZ ABRIR O INDEX NO ROOT
 @app.get("/")
-def home():
-    return {"status": "API rodando"}
+def serve_index():
+    return FileResponse("static/index.html")
 
 @app.post("/cadastro")
 def cadastrar_lead(lead: Lead):
